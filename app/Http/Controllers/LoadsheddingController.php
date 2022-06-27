f5922faa3c2986a1096bf6b9d378b5464727e1e6
Mqondisi <57135780+Mqoey@users.noreply.github.com>
2022-06-27 11:04:26 +0200

    add new modules

---
+++ app/Http/Controllers/LoadsheddingController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Loadshedding;
+use Illuminate\Http\Request;
+
+class LoadsheddingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Loadshedding  $loadshedding
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Loadshedding $loadshedding)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Loadshedding  $loadshedding
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Loadshedding $loadshedding)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Loadshedding  $loadshedding
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Loadshedding $loadshedding)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Loadshedding  $loadshedding
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Loadshedding $loadshedding)
+    {
+        //
+    }
+}
